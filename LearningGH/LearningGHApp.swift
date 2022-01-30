18ea4cc3f063b7a32df0bad2062d459450248f66
spradooooo <sprado.hak@gmail.com>
2022-01-30 11:09:23 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Mohamed Abdelhak on 30.01.22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
